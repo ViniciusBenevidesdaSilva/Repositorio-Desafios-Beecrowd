340665cc8f29ab7f382299df39099a43a1b392ed
Vinícius Benevides da Silva <vinishrafiel@gmail.com>
2022-09-15 17:04:16 -0300

    Desafio 1001 Extremanente Basico

---
+++ Iniciante/1001 Extremamente Basico.py
@@ -0,0 +1,37 @@
+
+# beecrowd | 1001: Extremamente Básico
+
+
+# Leia 2 valores inteiros e armazene-os nas variáveis A e B.
+# Efetue a soma de A e B atribuindo o seu resultado na variável X.
+# Imprima X conforme exemplo apresentado abaixo.
+
+# Entrada: A entrada contém 2 valores inteiros.
+# Saída: Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
+
+# Exemplos de Entrada	Exemplos de Saída
+# 10                    X = 19
+# 9
+
+# -10                   X = -6
+# 4
+
+# 15                    X = 8
+# -7
+#
+
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print(f'X = {X}')
+
+
+# PROBLEMA: 1001 - Extremamente Básico
+# RESPOSTA: Accepted
+# LINGUAGEM: Python 3.9
+# TEMPO: 0.180s
+# TAMANHO: 70 Bytes
+# MEMÓRIA: -
+# SUBMISSÃO: 15/09/2022 16:59:50
